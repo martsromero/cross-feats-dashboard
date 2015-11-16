f624aacb9640aee6000f10b0aa3ff0418353bc72
martsromero <pmr330@nyu.edu>
2015-11-16 18:25:16 -0500

    responsive

--- public/js/main.js
+++ public/js/main.js
@@ -319,38 +319,47 @@ function buildPrChart(){
 	};	
 
 	var options = {
-	//Boolean - Whether the scale should start at zero, or an order of magnitude down from the lowest value
-    scaleBeginAtZero : true,
+	 ///Boolean - Whether grid lines are shown across the chart
+    scaleShowGridLines : true,
 
-    //Boolean - Whether grid lines are shown across the chart
-    scaleShowGridLines : false,
+    //String - Colour of the grid lines
+    scaleGridLineColor : "rgba(0,0,0,.05)",
 
-    // //String - Colour of the grid lines
-    // scaleGridLineColor : "rgba(0,0,0,.05)",
-
-    // //Number - Width of the grid lines
-    // scaleGridLineWidth : 1,
+    //Number - Width of the grid lines
+    scaleGridLineWidth : 1,
 
     //Boolean - Whether to show horizontal lines (except X axis)
     scaleShowHorizontalLines: true,
 
     //Boolean - Whether to show vertical lines (except Y axis)
-    scaleShowVerticalLines: false,
+    scaleShowVerticalLines: true,
 
-    //Boolean - If there is a stroke on each bar
-    barShowStroke : true,
+    //Boolean - Whether the line is curved between points
+    bezierCurve : true,
 
-    //Number - Pixel width of the bar stroke
-    barStrokeWidth : 3,
+    //Number - Tension of the bezier curve between points
+    bezierCurveTension : 0.4,
 
-    //Number - Spacing between each of the X value sets
-    barValueSpacing : 80,
+    //Boolean - Whether to show a dot for each point
+    pointDot : true,
 
-    //Number - Spacing between data sets within X values
-    barDatasetSpacing : 3,
+    //Number - Radius of each point dot in pixels
+    pointDotRadius : 4,
 
-    responsive: true,
-	maintainAspectRatio: true,
+    //Number - Pixel width of point dot stroke
+    pointDotStrokeWidth : 1,
+
+    //Number - amount extra to add to the radius to cater for hit detection outside the drawn point
+    pointHitDetectionRadius : 20,
+
+    //Boolean - Whether to show a stroke for datasets
+    datasetStroke : true,
+
+    //Number - Pixel width of dataset stroke
+    datasetStrokeWidth : 2,
+
+    //Boolean - Whether to fill the dataset with a colour
+    datasetFill : true,
 
     //String - A legend template
     legendTemplate : "<ul class=\"<%=name.toLowerCase()%>-legend\"><% for (var i=0; i<datasets.length; i++){%><li><span style=\"background-color:<%=datasets[i].fillColor%>\"></span><%if(datasets[i].label){%><%=datasets[i].label%><%}%></li><%}%></ul>"
