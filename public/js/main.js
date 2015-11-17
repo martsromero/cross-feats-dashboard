9b8de888045ce52a74b094465694df0964e78fd2
martsromero <pmr330@nyu.edu>
2015-11-16 23:29:01 -0500

    Edit

--- public/js/main.js
+++ public/js/main.js
@@ -319,47 +319,38 @@ function buildPrChart(){
 	};	
 
 	var options = {
-	 ///Boolean - Whether grid lines are shown across the chart
-    scaleShowGridLines : true,
+	//Boolean - Whether the scale should start at zero, or an order of magnitude down from the lowest value
+	    scaleBeginAtZero : true,
 
-    //String - Colour of the grid lines
-    scaleGridLineColor : "rgba(0,0,0,.05)",
+	    //Boolean - Whether grid lines are shown across the chart
+	    scaleShowGridLines : true,
 
-    //Number - Width of the grid lines
-    scaleGridLineWidth : 1,
+	    //String - Colour of the grid lines
+	    scaleGridLineColor : "rgba(0,0,0,.05)",
 
-    //Boolean - Whether to show horizontal lines (except X axis)
-    scaleShowHorizontalLines: true,
+	    //Number - Width of the grid lines
+	    scaleGridLineWidth : 1,
 
-    //Boolean - Whether to show vertical lines (except Y axis)
-    scaleShowVerticalLines: true,
+	    //Boolean - Whether to show horizontal lines (except X axis)
+	    scaleShowHorizontalLines: true,
 
-    //Boolean - Whether the line is curved between points
-    bezierCurve : true,
+	    //Boolean - Whether to show vertical lines (except Y axis)
+	    scaleShowVerticalLines: true,
 
-    //Number - Tension of the bezier curve between points
-    bezierCurveTension : 0.4,
+	    //Boolean - If there is a stroke on each bar
+	    barShowStroke : true,
 
-    //Boolean - Whether to show a dot for each point
-    pointDot : true,
+	    //Number - Pixel width of the bar stroke
+	    barStrokeWidth : 3,
 
-    //Number - Radius of each point dot in pixels
-    pointDotRadius : 4,
+	    //Number - Spacing between each of the X value sets
+	    barValueSpacing : 30,
 
-    //Number - Pixel width of point dot stroke
-    pointDotStrokeWidth : 1,
+	    //Number - Spacing between data sets within X values
+	    barDatasetSpacing : 1,
 
-    //Number - amount extra to add to the radius to cater for hit detection outside the drawn point
-    pointHitDetectionRadius : 20,
-
-    //Boolean - Whether to show a stroke for datasets
-    datasetStroke : true,
-
-    //Number - Pixel width of dataset stroke
-    datasetStrokeWidth : 2,
-
-    //Boolean - Whether to fill the dataset with a colour
-    datasetFill : true,
+	    responsive: true,
+		maintainAspectRatio: true,
 
     //String - A legend template
     legendTemplate : "<ul class=\"<%=name.toLowerCase()%>-legend\"><% for (var i=0; i<datasets.length; i++){%><li><span style=\"background-color:<%=datasets[i].fillColor%>\"></span><%if(datasets[i].label){%><%=datasets[i].label%><%}%></li><%}%></ul>"
