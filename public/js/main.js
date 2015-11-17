e136fe80352e12d8287dd82a8838076701f139e2
martsromero <pmr330@nyu.edu>
2015-11-17 17:58:11 -0500

    crossfeats

--- public/js/main.js
+++ public/js/main.js
@@ -352,6 +352,8 @@ function buildPrChart(){
 	    responsive: true,
 		maintainAspectRatio: true,
 
+		
+
     //String - A legend template
     legendTemplate : "<ul class=\"<%=name.toLowerCase()%>-legend\"><% for (var i=0; i<datasets.length; i++){%><li><span style=\"background-color:<%=datasets[i].fillColor%>\"></span><%if(datasets[i].label){%><%=datasets[i].label%><%}%></li><%}%></ul>"
 		
